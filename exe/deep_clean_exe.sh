37836f4adf10d04ee7cb1795dfd002312c03b7c3
Dung-Ru Tsai <octobersky.tw@gmail.com>
2018-09-21 18:15:20 +0800

    Change the name

--- exe/deep_clean_exe.sh
+++ exe/deep_clean_exe.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -x
 LOCAL_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SIRIUS_HOME_PATH=$(echo $LOCAL_FILE_DIR | sed 's/\/exe//g')
-cd $SIRIUS_HOME_PATH/exe/HIL/master
+SIM_HOME_PATH=$(echo $LOCAL_FILE_DIR | sed 's/\/exe//g')
+cd $SIM_HOME_PATH/exe/HIL/master
 make clean
-cd $SIRIUS_HOME_PATH/exe/HIL/slave
+cd $SIM_HOME_PATH/exe/HIL/slave
 make clean
-cd $SIRIUS_HOME_PATH/exe/HIL/simgen_sample_code
+cd $SIM_HOME_PATH/exe/HIL/simgen_sample_code
 make clean
-cd $SIRIUS_HOME_PATH/exe/HIL/sdt_sample_code
+cd $SIM_HOME_PATH/exe/HIL/sdt_sample_code
 make clean
-cd $SIRIUS_HOME_PATH/exe/HIL/ratetable_sample_code
+cd $SIM_HOME_PATH/exe/HIL/ratetable_sample_code
 make clean
-cd $SIRIUS_HOME_PATH/exe/HIL/fc_can_test
+cd $SIM_HOME_PATH/exe/HIL/fc_can_test
 make clean
-cd $SIRIUS_HOME_PATH/exe/PIL/master
+cd $SIM_HOME_PATH/exe/PIL/master
 make clean
-cd $SIRIUS_HOME_PATH/exe/PIL/slave
+cd $SIM_HOME_PATH/exe/PIL/slave
 make clean
-cd $SIRIUS_HOME_PATH/exe/SIL/master
+cd $SIM_HOME_PATH/exe/SIL/master
 make clean
-cd $SIRIUS_HOME_PATH/exe/SIL/slave
+cd $SIM_HOME_PATH/exe/SIL/slave
 make clean
-$SIRIUS_HOME_PATH/exe/xil_common/script/clean_script.sh $SIRIUS_HOME_PATH/exe/
+$SIM_HOME_PATH/exe/xil_common/script/clean_script.sh $SIM_HOME_PATH/exe/
 set +x
